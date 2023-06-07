4f0de414ded1a5845fab5a586fd17f0aa90fb8ce
huseyinharunozkan <huseyinharunozkan@gmail.com>
2023-06-07 23:33:55 +0300

    Final Commit
    Final Commit

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -9,34 +9,124 @@ public class Fish : MonoBehaviour
     int angle;
     int maxAngle = 20;
     int minAngle = -60;
+    public Score score;
+    bool touchedGround;
+    public GameManager gameManager;
+    public Sprite fishDied;
+    SpriteRenderer sp;
+    Animator anim;
+    public ObstacleSpawner obstaclespawner;
+    [SerializeField] private AudioSource swim,hit,point;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _rb.velocity = new Vector2(_rb.velocity.x,9f);
+        _rb.gravityScale = 0;
+        sp = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            _rb.velocity = new Vector2(_rb.velocity.x, speed);
-        }
+        FishSwim();
+    }
 
-        if(_rb.velocity.y > 0)
+    private void FixedUpdate()
+    {
+        FishRotation();
+    }
+    void FishRotation()
+    {
+        if (_rb.velocity.y > 0)
         {
-            if(angle<= maxAngle)
+            if (angle <= maxAngle)
             {
                 angle = angle + 4;
             }
         }
-        else if(_rb.velocity.y < -2.5f)
+        else if (_rb.velocity.y < -2.5f)
         {
-            if(angle > minAngle)
+            if (angle > minAngle)
             {
                 angle = angle - 2;
             }
         }
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        if (touchedGround == false)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+           
+       
+        
+           
+        
+    }
+    void FishSwim()
+    {
+        if (Input.GetMouseButtonDown(0) && GameManager.gameOver == false )
+        {
+            swim.Play();
+            if (GameManager.gameStarted ==false)
+            {
+                _rb.gravityScale = 4f;
+                _rb.velocity = Vector2.zero;
+                _rb.velocity = new Vector2(_rb.velocity.x, speed);
+                obstaclespawner.InstantiateObstacle();
+                gameManager.GameHasStarted();
+            }
+            else
+            {
+                _rb.velocity = Vector2.zero;
+                _rb.velocity = new Vector2(_rb.velocity.x, speed);
+            }
+           
+        }
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            //Debug.Log("Score");
+            score.Scored();
+            point.Play();
+
+        }
+        else if (collision.CompareTag("Column") && GameManager.gameOver == false)
+        {
+            //game over
+            FishDieEffect();
+            gameManager.GameOver();
+        }
+    }
+    void FishDieEffect()
+    {
+        hit.Play();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (GameManager.gameOver == false)
+            {
+                //game over
+                FishDieEffect();
+                gameManager.GameOver();
+                GameOver();
+            }
+            else
+            {
+                //game over(fish)
+                GameOver();
+            }
+        }
+    }
+    void GameOver()
+    {
+        touchedGround = true;
+        transform.rotation = Quaternion.Euler(0, 0, -90);
+        sp.sprite = fishDied;
+        anim.enabled = false;
     }
 }
--- Assets/Scripts/LeftMovement.cs
+++ Assets/Scripts/LeftMovement.cs
@@ -7,23 +7,49 @@ public class LeftMovement : MonoBehaviour
     public float speed;
     BoxCollider2D box;
     float groundWidht;
+    float obstacleWidth;
    
     void Start()
     {
-        box = GetComponent<BoxCollider2D>();
-        groundWidht = box.size.x;
-        Debug.Log(groundWidht);
+        if (gameObject.CompareTag("Ground"))
+        {
+            box = GetComponent<BoxCollider2D>();
+            groundWidht = box.size.x;
+        }
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            obstacleWidth = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x;
+        }
+    
+        
     }
 
    
     void Update()
     {
-        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
-
-        if(transform.position.x <= -groundWidht)
+        if (GameManager.gameOver == false)
         {
-            transform.position = new Vector2(transform.position.x + 2 * groundWidht, transform.position.y);
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
         }
+       
+        if (gameObject.CompareTag("Ground"))
+        {
+            if (transform.position.x <= -groundWidht)
+            {
+                transform.position = new Vector2(transform.position.x + 2 * groundWidht, transform.position.y);
+            }
+        }
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            if (transform.position.x <GameManager.bottomLeft.x - obstacleWidth)
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+        
+
+        
 
     }
 }
